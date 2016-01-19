498222b0b406b89ee7de7d6db8656fdb3da5e0ae
Arjun Balaji <arjunblj@gmail.com>
2016-01-18 22:57:53 -0500

    Drop Debug mode for Heroku push

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def hello():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
